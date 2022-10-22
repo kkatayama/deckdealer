643d92fb4050a0508e96370db6b2e5e46accdd85
kkatayama <kkatayama@udel.edu>
2022-10-21 20:19:08 -0400

    cleanup...

--- tests/convert_mkdown_tables_to_html.py
+++ tests/convert_mkdown_tables_to_html.py
@@ -11,7 +11,7 @@ r = re.compile(regex)
 in_table = False
 for line in readme.splitlines():
     if re.search(r'# \d+\.', line):
-        print(line)
+        print(f'{"-"*80}\n{line}\n{"-"*80}')
     if r.search(line):
         items = r.findall(line)
         if in_table == False:
@@ -21,18 +21,21 @@ for line in readme.splitlines():
         #table += '<tr>\n'
         row = '<tr>'
         for i, item in enumerate(items):
-            if item[0].isupper():
+            if item[0].isupper() and item[-1].islower():
                 headers += f'<td> {item} </td>'
             elif ':--' in item or '--:' in item:
                 if i == 0:
                     headers += '</tr>\n'
             else:
                 item = item.replace('**', '').replace('`', '')
-                if '?' in item:                                              # is params
+                if '?' in item:
+                    # is params
                     row += f'\n<td>\n\n```erlang\n{item}\n```\n\n</td>'
-                elif item.strip().startswith('/'):                           # is url_path
+                elif item.strip().startswith('/'):
+                    # is url_path
                     row += f'\n<td>\n\n```jq\n{item}\n```\n\n</td>'
-                else:                                                        # is statement
+                else:
+                    # is statement
                     row += f'\n<td>\n\n```rexx\n{item}\n```\n\n</td>'
         row += '\n</tr>\n'
         if len(row) > 11:
@@ -43,4 +46,4 @@ for line in readme.splitlines():
             in_table = False
             table += '</table>\n'
             print(table)
-            
+""
