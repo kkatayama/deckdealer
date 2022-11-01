a2adb9f9b31255fe0baef66911ae9567869e5a41
kkatayama <katayama@udel.edu>
2022-11-01 19:12:07 -0400

    enable CORS

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def strip_path():
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 # -- index - response: running
 @route("/", method=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
