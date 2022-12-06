2d5d2863e1afc2aeff3b84e48e395fc5b355cc4c
kkatayama <katayama@udel.edu>
2022-12-06 12:10:02 -0500

    git auto update

--- js/game2.js
+++ js/game2.js
@@ -2,18 +2,21 @@
 //                              Global Variables                             //
 ///////////////////////////////////////////////////////////////////////////////
 var api_url = "https://deckdealer.hopto.org"
-var user_id = "";
+
+///////////////////////////////////////////////////////////////////////////////
+//                      Global Variables (for debugging)                     //
+///////////////////////////////////////////////////////////////////////////////
+// var user_id = "";
+// var game_list = [];
 
 ///////////////////////////////////////////////////////////////////////////////
 //                              Global Functions                             //
 ///////////////////////////////////////////////////////////////////////////////
 function getUserID() {
+  /* GET: https://deckdealer.hopto.org/status */
   var url = new URL('/status', api_url).toString();
   var temp_id = "";
-  $.ajax({
-    url: url,
-    type: 'get',
-    async: false,
+  $.ajax({url: url, type: 'get', async: false,
     success: function(data) {
       temp_id = data.user_id;
     }
@@ -21,7 +24,21 @@ function getUserID() {
   return temp_id;
 }
 
+function getGameList() {
+  /* GET: https://deckdealer.hopto.org/get/games */
+  var url = new URL('/get/games', api_url).toString();
+  var temp_list = [];
+  $.ajax({url: url, type: 'get', async: false,
+    success: function(data) {
+      temp_list = data.data;
+    }
+  });
+  return temp_list;
+}
+
 $(document).ready(function() {
-  user_id = getUserID();
+  var user_id   = getUserID();
+  var game_list = getGameList();
   console.log('user_id = ' + user_id);
+  console.table(game_list);
 });
