209fc890686927d3048fad5418d9fd9ed97704d8
kkatayama <katayama@udel.edu>
2022-12-06 12:00:04 -0500

    git auto update

--- js/game2.js
+++ js/game2.js
@@ -3,21 +3,25 @@
 ///////////////////////////////////////////////////////////////////////////////
 var api_url = "https://deckdealer.hopto.org"
 var user_id = "";
-var uid = "";
 
 ///////////////////////////////////////////////////////////////////////////////
 //                              Global Functions                             //
 ///////////////////////////////////////////////////////////////////////////////
 function getUserID() {
   var url = new URL('/status', api_url).toString();
-  return $.get(url, function(data) {
-    user_id = data.user_id;
-    console.log('user_id = ' + user_id);
-    return user_id;
-  })
+  $.ajax({
+    url: url,
+    type: 'get',
+    async: false,
+    success: function(data) {
+      user_id = data.user_id;
+      console.log('user_id = ' + user_id);
+    }
+  });
+  return user_id;
 }
 
 $(document).ready(function() {
-  uid = getUserID();
+  var uid = getUserID();
   console.log('uid = ' + uid);
 });
