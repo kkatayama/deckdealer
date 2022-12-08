3f0e6eccd29af47d6ecbf602e979af7f13159d4d
kkatayama <katayama@udel.edu>
2022-12-07 21:20:01 -0500

    git auto update

--- js/game-blackjack2.js
+++ js/game-blackjack2.js
@@ -12,7 +12,6 @@ var user_name = "";
 var players = [];
 var active = [];
 var card_index = 1;
-var player_index = 1;
 
 /* taken from: https://stackoverflow.com/questions/18673860/defining-a-html-template-to-append-using-jquery */
 var player_template = ({ num_cols, info, cards }) => `
@@ -135,6 +134,15 @@ function getActiveGame() {
   return temp_active;
 }
 
+function renderPlayerTemplate() {
+  num_cols = (num_cards > 4) ? 8 : 6;
+  var html_info = [{ player_id: "2", user_name: "alice", score: 20 },].map(player_info_template).join('');
+  var html_cards = [{ img: "6S.png" }, { img: "6H.png" }].map(player_cards_template).join('');
+  var html_player = [{ num_cols: 6, info: html_info, cards: html_cards }].map(player_template).join('');
+  $('#players').append(html_player);
+}
+
+
 function printActiveGame() {
   active_game = getActiveGame();
 
@@ -145,42 +153,8 @@ function printActiveGame() {
 
   } else {
     showPopup("Waiting for the dealer to start...");
-  }
-}
 
-function printPlayerList() {
-  player_list = getPlayerList();
-
-  if ((num_players < player_list.length) || (num_players > player_list.length)) {
-    num_players = player_list.length;
-    $('#game-queue').text(game_name + ' Player Queue');
-    $('#player-list').html(html);
-    for (var i = 0; i < player_list.length; i++) {
-      var player = player_list[i];
-      $('#player-list').append(
-        '<div class="row justify-content-center text-success">' +
-          '<div class="col-1 bg-light ps-3 text-center border" style="--bs-bg-opacity: .6;">' +
-          '<h3>' + player.player_id +'</h3>' +
-          '</div>' +
-          '<div class="col-3 bg-light ps-3 border" style="--bs-bg-opacity: .6;">' +
-          '<h3>' + player.name +'</h3>' +
-          '</div>' +
-          '<div class="col-4 bg-light ps-3 border" style="--bs-bg-opacity: .6;">' +
-          '<h3>' + player.email +'</h3>' +
-          '</div>' +
-          '</div>'
-      )
-    }
-    if (player_list.length < min_players) {
-      $('#status').html('<p class="lh-base">waiting for additional players...</p>')
-    } else {
-      $('#status').removeClass('bg-light');
-      $('#status').removeClass('border');
-      $('#status').removeClass('rounded');
-      $('#status').html(
-        '<div class="btn btn-primary form-control btn-block">Start Game</div>'
-      )
-    }
+    if ()
   }
 }
 
