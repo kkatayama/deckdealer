40245da4e9be0481d287c538f365a4c7caf6dc2f
bcleslie <bcleslie@udel.edu>
2023-04-24 16:09:26 -0400

    Revert

--- game-blackjack.js
+++ game-blackjack.js
@@ -650,9 +650,6 @@ function closeGame() {
       window.location.href = "index.html";
     })
   }
-  else {
-    window.location.href = "index.html";
-  }
 }
 
 function startTimer(ms=5000, callback) {
